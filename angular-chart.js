afbe589112420b04344b4e2b3071949a5e808351
Jerome Touffe-Blin <jtblin@atlassian.com>
2014-07-20 23:34:58 +1000

    Change series labels setting, clean-up colours and add to Chart.defaults.global.colours to allow change

--- angular-chart.js
+++ angular-chart.js
@@ -3,14 +3,14 @@
 
   Chart.defaults.global.responsive = true;
 
-  var config = [
+  Chart.defaults.global.colours = [
     { // blue
       fillColor: "rgba(151,187,205,0.2)",
       strokeColor: "rgba(151,187,205,1)",
       pointColor: "rgba(151,187,205,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(151,187,205,1)"
+      pointHighlightStroke: "rgba(151,187,205,0.8)"
     },
     { // light grey
       fillColor: "rgba(220,220,220,0.2)",
@@ -18,47 +18,47 @@
       pointColor: "rgba(220,220,220,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)"
+      pointHighlightStroke: "rgba(220,220,220,0.8)"
     },
     { // red
-      fillColor: "rgba(247, 70, 74, 0.2)",
-      strokeColor: "rgba(247, 70, 74, 1)",
-      pointColor: "rgba(247, 70, 74, 1)",
+      fillColor: "rgba(247,70,74,0.2)",
+      strokeColor: "rgba(247,70,74,1)",
+      pointColor: "rgba(247,70,74,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(247, 70, 74, 1)"
-    },
-    { // yellow
-      fillColor: "rgba(70, 191, 189, 0.2)",
-      strokeColor: "rgba(70, 191, 189, 1)",
-      pointColor: "rgba(70, 191, 189, 1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(70, 191, 189, 1)"
+      pointHighlightStroke: "rgba(247,70,74,0.8)"
     },
     { // green
-      fillColor: "rgba(253, 180, 92, 0.2)",
-      strokeColor: "rgba(253, 180, 92, 1)",
-      pointColor: "rgba(253, 180, 92, 1)",
+      fillColor: "rgba(70,191,189,0.2)",
+      strokeColor: "rgba(70,191,189,1)",
+      pointColor: "rgba(70,191,189,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(253, 180, 92, 1)"
+      pointHighlightStroke: "rgba(70,191,189,0.8)"
+    },
+    { // yellow
+      fillColor: "rgba(253,180,92,0.2)",
+      strokeColor: "rgba(253,180,92,1)",
+      pointColor: "rgba(253,180,92,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(253,180,92,0.8)"
     },
     { // grey
-      fillColor: "rgba(148, 159, 177, 0.2)",
-      strokeColor: "rgba(148, 159, 177, 1)",
-      pointColor: "rgba(148, 159, 177, 1)",
+      fillColor: "rgba(148,159,177,0.2)",
+      strokeColor: "rgba(148,159,177,1)",
+      pointColor: "rgba(148,159,177,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(148, 159, 177, 1)"
+      pointHighlightStroke: "rgba(148,159,177,0.8)"
     },
     { // dark grey
-      fillColor: "rgba(77, 83, 96, 0.2)",
-      strokeColor: "rgba(77, 83, 96, 1)",
-      pointColor: "rgba(77, 83, 96, 1)",
+      fillColor: "rgba(77,83,96,0.2)",
+      strokeColor: "rgba(77,83,96,1)",
+      pointColor: "rgba(77,83,96,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(77, 83, 96, 1)"
+      pointHighlightStroke: "rgba(77,83,96,1)"
     }
   ];
 
@@ -78,6 +78,7 @@
         data: '=',
         labels: '=',
         options: '=',
+        series: '=',
         legend: '@',
         click: '='
       },
@@ -85,7 +86,7 @@
         var chart;
 
         scope.$watch('data', function (newVal, oldVal) {
-          if (hasDataSets(type) && Array.isArray(newVal[0]) && ! newVal[0].length) return;
+          if (hasDataSets(type) && ! newVal[0].length) return;
           if (! chart) {
             chart = getGraph(type, scope);
             if (scope.legend) {
@@ -113,7 +114,7 @@
 
   function getGraph (type, scope) {
     var cvs = document.getElementById(scope.id), ctx = cvs.getContext("2d");
-    var data = hasDataSets(type) ? getDataSets(scope.labels, scope.data) : getData(scope.labels, scope.data);
+    var data = hasDataSets(type) ? getDataSets(scope.labels, scope.data, scope.series || []) : getData(scope.labels, scope.data);
     var chart = new Chart(ctx)[type](data, scope.options || {});
     if (scope.click) {
       cvs.onclick = function (evt) {
@@ -130,18 +131,13 @@
     return ['Line', 'Bar', 'Radar'].indexOf(type) > -1;
   }
 
-  function getDataSets (labels, data) {
+  function getDataSets (labels, data, series) {
     return {
       labels: labels,
       datasets: data.map(function (item, i) {
-        var dataSet = clone(config[i]);
-        if (! Array.isArray(item)) {
-          var key = Object.keys(item)[0];
-          dataSet.label = key;
-          dataSet.data = item[key];
-        } else {
-          dataSet.data = item;
-        }
+        var dataSet = clone(Chart.defaults.global.colours[i]);
+        dataSet.label = series[i];
+        dataSet.data = item;
         return dataSet;
       })
     };
@@ -160,8 +156,8 @@
       return {
         label: label,
         value: data[i],
-        color: config[i].strokeColor,
-        highlight: config[i].pointHighlightStroke
+        color: Chart.defaults.global.colours[i].strokeColor,
+        highlight: Chart.defaults.global.colours[i].pointHighlightStroke
       };
     });
   }
