eee6a15dc4ed10611a6a96b62346f3a6b65ae4c3
Jerome Touffe-Blin <jtblin@atlassian.com>
2014-07-21 20:49:44 +1000

    Complete charts examples

--- examples/app.js
+++ examples/app.js
@@ -44,13 +44,11 @@
 
   app.controller("PieCtrl", function ($scope) {
     $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-
     $scope.data = [300, 500, 100];
   });
 
   app.controller("PolarAreaCtrl", function ($scope) {
     $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
-
     $scope.data = [300, 500, 100, 40, 120];
   });
 
