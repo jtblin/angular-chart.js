f516d182fdb219288e59dbede766d848347a7933
Jerome Touffe-Blin <jtblin@gmail.com>
2014-10-16 23:18:55 +1100

    Bump version

--- gulpfile.js
+++ gulpfile.js
@@ -25,9 +25,20 @@
       .pipe(jshint.reporter(stylish));
   });
 
-  gulp.task('bump', function () {
+  gulp.task('bump-major', function () {
+    return gulp.src(['./package.json', './bower.json'])
+      .pipe(bump({type:'major'}))
+      .pipe(gulp.dest('./'));
+  });
+  gulp.task('bump-minor', function () {
     return gulp.src(['./package.json', './bower.json'])
       .pipe(bump({type:'minor'}))
+      .pipe(gulp.dest('./'));
+  });
+
+  gulp.task('bump-patch', function () {
+    return gulp.src(['./package.json', './bower.json'])
+      .pipe(bump({type:'patch'}))
       .pipe(gulp.dest('./'));
   });
 
