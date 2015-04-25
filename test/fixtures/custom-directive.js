64e0155a9cb6102c303c867f76e49e963668fd88
Jerome Touffe-Blin <jtblin@gmail.com>
2015-04-25 16:39:49 +1000

    Fix custom directive test (typo)

--- test/fixtures/custom-directive.js
+++ test/fixtures/custom-directive.js
@@ -3,7 +3,7 @@
 
   var app = angular.module('pie', ['chart.js']);
 
-  app.directive('mySpecialPie', function (ChatJsFactory) { return new ChatJsFactory('Pie'); });
+  app.directive('mySpecialPie', function (ChartJsFactory) { return new ChartJsFactory('Pie'); });
 
   app.controller('PieCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
     $scope.labels = ['Series A', 'Series B'];
