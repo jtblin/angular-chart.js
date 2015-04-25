56838c139b22b944049e9c56c0160b211d65130c
Jerome Touffe-Blin <jtblin@gmail.com>
2015-04-25 17:28:02 +1000

    Fix dataset fill for line charts in examples

--- examples/app.js
+++ examples/app.js
@@ -9,9 +9,9 @@
       colours: ['#97BBCD', '#DCDCDC', '#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
       responsive: true
     });
-    // Configure all line charts
-    ChartJsProvider.setOptions('Line', {
-      datasetFill: false
+    // Configure all doughnut charts
+    ChartJsProvider.setOptions('Doughnut', {
+      animateScale: true
     });
   });
 
