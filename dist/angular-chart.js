e2c923949f5c9b27557d837c89a237e886ee793c
Jerome Touffe-Blin <jtblin@gmail.com>
2015-09-08 06:01:04 +0700

    0.8.3

--- dist/angular-chart.js
+++ dist/angular-chart.js
@@ -1,11 +1,11 @@
 (function (factory) {
   'use strict';
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['angular', 'chart'], factory);
-  } else if (typeof exports === 'object') {
+  if (typeof exports === 'object') {
     // Node/CommonJS
     module.exports = factory(require('angular'), require('chart.js'));
+  }  else if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['angular', 'chart'], factory);
   } else {
     // Browser globals
     factory(angular, Chart);
