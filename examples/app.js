edacb1e14788e30d0d3f1a8bc6ea885281e06418
Jerome Touffe-Blin <jtblin@gmail.com>
2016-04-15 21:10:47 +1000

    Remove custom directive and use new stacked bar option

--- examples/app.js
+++ examples/app.js
@@ -119,6 +119,17 @@
   app.controller('StackedBarCtrl', function ($scope) {
     $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     $scope.type = 'StackedBar';
+    $scope.series = ['2015', '2016'];
+    $scope.options = {
+      scales: {
+        xAxes: [{
+          stacked: true,
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    };
 
     $scope.data = [
       [65, 59, 90, 81, 56, 55, 40],
