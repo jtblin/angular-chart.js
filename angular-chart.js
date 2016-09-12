0a75cff668fbe4dbe1708988b70997a52e4b99ea
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-09-12 15:21:09 -0400

    Rewrote getColors in angular-chart.js so that RGB and RGBA colors may be input as strings

--- angular-chart.js
+++ angular-chart.js
@@ -226,11 +226,8 @@
     }
 
     function convertColor (color) {
-      // Allows RGB colors to be input as an array: [r, g, b]
-      if (typeof color === 'object' && color !== null && color.length === 3) return getColor(color);
-      // Allows a RGBA to be input as an array: [r, g, b, a]. Also sets the a value for 
-      // pointBackgroundColor, borderColor, and pointHoverBorderColor
-      if (typeof color === 'object' && color !== null && color.length === 4) return getRGBAColor(color);
+      // Allows RGB and RGBA colors to be input as a string: e.g.: "rgb(159,204,0)", "rgba(159,204,0, 0.5)"
+      if (typeof color === 'string' && color[0] === 'r') return getColor(color);
       // Allows hex colors to be input as a string.
       if (typeof color === 'string' && color[0] === '#') return getColor(hexToRgb(color.substr(1)));
       // Allows colors to be input as an object, bypassing getColor() entirely
@@ -244,26 +241,53 @@
     }
 
     function getColor (color) {
-      return {
-        backgroundColor: rgba(color, 0.2),
-        pointBackgroundColor: rgba(color, 1),
-        pointHoverBackgroundColor: rgba(color, 0.8),
-        borderColor: rgba(color, 1),
-        pointBorderColor: '#fff',
-        pointHoverBorderColor: rgba(color, 1)
+      function colorSetter(colorArray, aVal){
+        return {
+          backgroundColor: rgba(colorArray, 0.2),
+          pointBackgroundColor: rgba(colorArray, aVal),
+          pointHoverBackgroundColor: rgba(colorArray, 0.8),
+          borderColor: rgba(colorArray, aVal),
+          pointBorderColor: '#fff',
+          pointHoverBorderColor: rgba(colorArray, aVal)
+        };
       };
-    }
-
-    function getRGBAColor(color){
-      return {
-        backgroundColor: rgba(color.slice(0,3), 0.2),
-        pointBackgroundColor: rgba(color.slice(0,3), color[3]),
-        pointHoverBackgroundColor: rgba(color.slice(0,3), 0.8),
-        borderColor: rgba(color.slice(0,3), color[3]),
-        pointBorderColor: '#fff',
-        pointHoverBorderColor: rgba(color.slice(0,3), color[3])
-      };      
-    }
+      // If the color is a Hex color, this processes the array that hexToRgb() returns.
+      if (typeof color === 'object'){
+        return colorSetter(color, 1);
+      }
+      if (typeof color === 'string'){
+        // If the color is an RGB/RGBA color, this finds the indexes of all commas in the string, then converts
+        // the RGBA or RGB string into an array for processing by the rgba() function when it tests the browser for RGBA color support.
+        var rgbaArray=[];
+        var commaIndexes = [];
+        var position = color.indexOf(',');
+        while (position !== -1) {
+          commaIndexes.push(position);
+          position = color.indexOf(',', position + 1);
+        };
+        // Converts the first RGB/RGBA number in the string into a number and pushes it into the array.
+        if (commaIndexes.length === 2) {
+          rgbaArray.push(color.substring(4, commaIndexes[0]));
+        } else {
+          rgbaArray.push(color.substring(5, commaIndexes[0]));
+        };
+        // Converts the second RGB/RGBA numbers in the string into numbers and pushes them into the array
+        rgbaArray.push(color.substring(commaIndexes[0]+1, commaIndexes[1]));
+        // Converts the third RGB/RGBA number in the string into a number and pushes it into the array.          
+        if (commaIndexes.length === 2){
+          rgbaArray.push(color.substring(commaIndexes[1]+1, color.length-1)); 
+        // Sets the A value for an RGB color to 1.
+          rgbaArray.push(1);        
+        } else {
+          rgbaArray.push(color.substring(commaIndexes[1]+1, commaIndexes[2]));
+        // If RGBA number is used, converts the fourth RGBA number in the string into a number and pushes it into the array.                      
+          rgbaArray.push(color.substring(commaIndexes[2]+1, color.length-1));  
+        };         
+        color = rgbaArray.slice(0,3);
+        var aVal = rgbaArray[3];
+        return colorSetter(color, aVal); 
+      };
+    };
 
     function getRandomInt (min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
