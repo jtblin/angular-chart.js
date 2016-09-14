7aec7b417ca2032dfcb470969c0ec7d7bc6a2e38
Shubham Dhond <shubham@eventmobi.com>
2016-09-14 08:03:39 -0400

    Fix for global configuration overrides (Issue 449) (#498)
    * Fix for global configuration overrides (Issue 449)

---
+++ test/fixtures/options-override.js
@@ -0,0 +1,73 @@
+(function () {
+  'use strict';
+
+  var app = angular.module('charts', ['chart.js']);
+  
+  app.config(function (ChartJsProvider) {
+    ChartJsProvider.setOptions('global', {
+      legend: {
+        display: true
+      }
+    });
+  });
+
+  app.controller('LineCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $timeout(function () {
+      $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+      $scope.data = [
+        [28, 48, 40, 19, 86, 27, 90],
+        [65, 59, 80, 81, 56, 55, 40]
+      ];
+      $scope.series = ['Series C', 'Series D'];
+    }, 0);
+  }]);
+
+  app.controller('BarCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.options = { scaleShowVerticalLines: false };
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $timeout(function () {
+      $scope.options = { scaleShowVerticalLines: true };
+    }, 0);
+  }]);
+
+  app.controller('DoughnutCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.labels = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+    $scope.data = [0, 0, 0];
+    // TODO: investigate why chart was not showing up without this hack
+    $timeout(function () {
+      $scope.data = [350, 450, 100];
+    }, 0);
+  }]);
+
+  app.controller('PieCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.labels = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+    $scope.data = [0, 0, 0];
+    $timeout(function () {
+      $scope.data = [350, 450, 100];
+    }, 0);
+  }]);
+
+  app.controller('PolarAreaCtrl', function ($scope) {
+    $scope.labels = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
+    $scope.data = [300, 500, 100, 40, 120];
+  });
+
+  app.controller('RadarCtrl', function ($scope) {
+    $scope.labels = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+    $scope.data = [
+      [65, 59, 90, 81, 56, 55, 40],
+      [28, 48, 40, 19, 96, 27, 100]
+    ];
+  });
+})();
